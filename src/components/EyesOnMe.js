6bead66ccabcff187c8ba30338c0b1e40ce168e8
Garrett <gcamoin5@gmail.com>
2023-03-28 15:39:22 -0400

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+
+
+function EyesonMe({focus, blur}) {
+    return <button onFocus={focus} onBlur={blur}>Eyes on me</button>
+
+    function focus() {
+        console.log("Good!")
+
+    }
+
+    function blur() {
+        console.log('Hey! Eyes on me!')
+    }
+
+}
+
+
+
+export default EyesonMe
